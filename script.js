8d85dab744c744332447163c6e8cb150e844629b
deymion <86213360+deymion@users.noreply.github.com>
2024-04-26 14:49:59 +0800

    Implement a "clear" button to clear the display

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let input = document.querySelector('#input');
+
+let btn = document.querySelector('#btnKeys');
+
+btn.addEventListener('click', (event) => {
+    let target = event.target;
+
+    console.log(target);
+    if(target.textContent === 'AC') clearDisplay();
+});
+
 const add = function (a, b) {
     return a + b;
 };
@@ -16,7 +27,12 @@ const divide = function (a, b) {
 
 function operate(operator, n1, n2) {
     return operator === "+" ? add(n1, n2)
-        : operator === "-" ? subtract(n1, n2) 
-        : operator === "x" ? multiply(n1, n2) 
-        : operator === "/" ? divide(n1, n2) : 'Unvalid Format';
+        : operator === "-" ? subtract(n1, n2)
+            : operator === "x" ? multiply(n1, n2)
+                : operator === "/" ? divide(n1, n2) : 'Unvalid Format';
 }
+
+function clearDisplay() {
+    return input.textContent = "";
+}
+
